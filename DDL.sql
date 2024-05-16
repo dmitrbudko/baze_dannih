7ce745473f18cb7676b3b8b764ca8d01e7c04baa
dmitrbudko <159508847+dmitrbudko@users.noreply.github.com>
2024-05-16 19:42:05 +0500

    Realizoval Item_Table

--- DDL.sql
+++ DDL.sql
@@ -3,12 +3,18 @@ CREATE TABLE Caliber_Table (
   	metrics VARCHAR(128) NOT NULL UNIQUE
 );
 
+CREATE TABLE Item_Table (
+	item_id BIGINT PRIMARY KEY NOT NULL
+);
+
 CREATE TABLE Price_Table (
 	price_id BIGINT PRIMARY KEY NOT NULL,
   	price INT NOT NULL,
+  	item_id INT NOT NULL,
   	date_start DATE NOT NULL, 
 	date_end DATE NOT NULL,
-  	CHECK (date_start <= date_end)
+  	CHECK (date_start <= date_end),
+  	CONSTRAINT item_id_fk FOREIGN KEY (item_id) REFERENCES item_table(item_id)
 );
 
 CREATE TABLE Rifle_Model_Table (
@@ -16,9 +22,9 @@ CREATE TABLE Rifle_Model_Table (
   	model_classification_name VARCHAR(128) NOT NULL UNIQUE,
   	hg_type VARCHAR(128) NOT NULL,
     caliber_id BIGINT,
-    price_id BIGINT,
+    item_id BIGINT NOT NULL,
   	CONSTRAINT caliber_id_fk FOREIGN KEY (caliber_id) REFERENCES caliber_table(caliber_id),
-  	CONSTRAINT price_id_fk FOREIGN KEY (price_id) REFERENCES price_table(price_id)
+  	CONSTRAINT item_id_fk FOREIGN KEY (item_id) REFERENCES item_table(item_id)
 );
 
 CREATE TABLE Muzzle_Break_Table (
@@ -26,18 +32,18 @@ CREATE TABLE Muzzle_Break_Table (
   	mb_type VARCHAR(128) NOT NULL,
   	mb_compatible_id BIGINT,
     caliber_id BIGINT,
-    price_id BIGINT,
+    item_id BIGINT NOT NULL,
  	CONSTRAINT caliber_id_fk FOREIGN KEY (caliber_id) REFERENCES caliber_table(caliber_id),
-  	CONSTRAINT price_id_fk FOREIGN KEY (price_id) REFERENCES price_table(price_id)
+  	CONSTRAINT item_id_fk FOREIGN KEY (item_id) REFERENCES item_table(item_id)
 );
 
 CREATE TABLE Mag_Table (
 	mag_id BIGINT PRIMARY KEY NOT NULL,
   	capacity INT NOT NULL,
     caliber_id BIGINT,
-    price_id BIGINT,
+    item_id BIGINT NOT NULL,
   	CONSTRAINT caliber_id_fk FOREIGN KEY (caliber_id) REFERENCES caliber_table(caliber_id),
-  	CONSTRAINT price_id_fk FOREIGN KEY (price_id) REFERENCES price_table(price_id)
+  	CONSTRAINT item_id_fk FOREIGN KEY (item_id) REFERENCES item_table(item_id)
 );
 
 CREATE TABLE Handguard_Table (
@@ -45,9 +51,9 @@ CREATE TABLE Handguard_Table (
   	mounts_count INT,
   	mount_type VARCHAR(128),
     caliber_id BIGINT,
-    price_id BIGINT,
+    item_id BIGINT NOT NULL,
   	CONSTRAINT hg_type_fk FOREIGN KEY (hg_type) REFERENCES rifle_model_table(hg_type),
-  	CONSTRAINT price_id_fk FOREIGN KEY (price_id) REFERENCES price_table(price_id)
+  	CONSTRAINT item_id_fk FOREIGN KEY (item_id) REFERENCES item_table(item_id)
 );
 
 CREATE TABLE Tactical_Modification_Table (
@@ -55,6 +61,7 @@ CREATE TABLE Tactical_Modification_Table (
   	mod_type VARCHAR(128) NOT NULL,
   	mount_type VARCHAR(128),
     mount_id BIGINT,
+  	item_id BIGINT NOT NULL,
   	CONSTRAINT price_id_fk FOREIGN KEY (mount_type) REFERENCES handguard_table(mount_type),
-  	CONSTRAINT mount_type_fk FOREIGN KEY (mount_id) REFERENCES price_table(price_id)
+  	CONSTRAINT item_id_fk FOREIGN KEY (item_id) REFERENCES item_table(item_id)
 );
